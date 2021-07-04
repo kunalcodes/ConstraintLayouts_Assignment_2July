a15faf6a2505780a5940711f396895ecc2a945be
kunalcodes <kunal.chaturvedi75@gmail.com>
2021-07-04 21:33:31 +0530

    Masai Launcher added

--- app/src/main/java/com/example/constraint_layouts_assignment/MainActivity.java
+++ app/src/main/java/com/example/constraint_layouts_assignment/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.constraint_layouts_assignment;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button mBtnMasaiLauncher;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.scrolling_article_layout);
+        setContentView(R.layout.launchmasai_layout);
+        mBtnMasaiLauncher = findViewById(R.id.btnMasaiLauncher);
+        mBtnMasaiLauncher.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("https://www.masaischool.com/"));
+                startActivity(intent);
+            }
+        });
     }
 }
